77ff56e60ce5b33c9c45c46dfebb9bc37d6df27b
Allison Oler <linoler@gmail.com>
2025-01-10 14:08:14 -0700

    Started working on the positions of pieces. Some tests that worked before now fail, but it is because there is more functionality that is necessary.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -24,23 +24,6 @@ public class ChessPosition {
         return this.row;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        ChessPosition that = (ChessPosition) o;
-        return row == that.row && col == that.col;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(row, col);
-    }
-
     /**
      * @return which column this position is in
      * 1 codes for the left row
