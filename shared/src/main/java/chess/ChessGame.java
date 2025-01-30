0d33b99b55c21b2cc16649060bcba993e07b718a
Allison Oler <linoler@gmail.com>
2025-01-29 22:43:29 -0700

    Tried to clean up code

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -14,6 +14,7 @@ public class ChessGame {
     private ChessBoard board;
 
 
+
     public ChessGame() {
         teamColor = TeamColor.WHITE;
         board = new ChessBoard();
@@ -134,6 +135,7 @@ public class ChessGame {
         return false;
 
     }
+
 
     /**
      * Determines if the given team is in checkmate
