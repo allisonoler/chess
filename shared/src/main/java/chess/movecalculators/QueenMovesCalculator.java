1c131828a3c3fd7f5637328fe70fd9c20862be57
Allison Oler <linoler@gmail.com>
2025-02-26 20:25:42 -0700

    fixed naming

--- shared/src/main/java/chess/movecalculators/QueenMovesCalculator.java
+++ shared/src/main/java/chess/movecalculators/QueenMovesCalculator.java
@@ -10,46 +10,12 @@ public class QueenMovesCalculator implements PieceMovesCalculator{
     @Override
     public ArrayList<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> possibleMoves = new ArrayList<ChessMove>();
-        for (int j = myPosition.getColumn()-1; j>0;j--) {
-            if (!PieceMovesCalculator.goodMove(board, myPosition, new ChessPosition(myPosition.getRow(), j), possibleMoves)) {
-                break;
-            }
-        }
-        for (int j = myPosition.getColumn()+1; j<9;j++) {
-            if (!PieceMovesCalculator.goodMove(board, myPosition, new ChessPosition(myPosition.getRow(), j), possibleMoves)) {
-                break;
-            }
-        }
-        for (int i = myPosition.getRow()-1; i>0;i--) {
-            if (!PieceMovesCalculator.goodMove(board, myPosition, new ChessPosition(i, myPosition.getColumn()), possibleMoves)) {
-                break;
-            }
-        }
-        for (int i = myPosition.getRow()+1; i<9;i++) {
-            if (!PieceMovesCalculator.goodMove(board, myPosition, new ChessPosition(i, myPosition.getColumn()), possibleMoves)) {
-                break;
-            }
-        }
-        for (int i = myPosition.getRow()+1, j = myPosition.getColumn()+1; i<9&&j<9;i++,j++) {
-            if (!PieceMovesCalculator.goodMove(board, myPosition, new ChessPosition(i,j), possibleMoves)) {
-                break;
-            }
-        }
-        for (int i = myPosition.getRow()+1, j = myPosition.getColumn()-1; i<9&&j>0;i++,j--) {
-            if (!PieceMovesCalculator.goodMove(board, myPosition, new ChessPosition(i,j), possibleMoves)) {
-                break;
-            }
-        }
-        for (int i = myPosition.getRow()-1, j = myPosition.getColumn()+1; i>0&&j<9;i--,j++) {
-            if (!PieceMovesCalculator.goodMove(board, myPosition, new ChessPosition(i,j), possibleMoves)) {
-                break;
-            }
-        }
-        for (int i = myPosition.getRow()-1, j = myPosition.getColumn()-1; i>0&&j>0;i--,j--) {
-            if (!PieceMovesCalculator.goodMove(board, myPosition, new ChessPosition(i,j), possibleMoves)) {
-                break;
-            }
-        }
+        RookMovesCalculator rookMovesCalculator = new RookMovesCalculator();
+        ArrayList<ChessMove> straightMoves = rookMovesCalculator.pieceMoves(board, myPosition);
+        BishopMovesCalculator bishopMovesCalculator = new BishopMovesCalculator();
+        ArrayList<ChessMove> diagonalMoves = bishopMovesCalculator.pieceMoves(board, myPosition);
+        possibleMoves.addAll(straightMoves);
+        possibleMoves.addAll(diagonalMoves);
         return possibleMoves;
     }
 }
