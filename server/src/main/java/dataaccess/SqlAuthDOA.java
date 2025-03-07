c7174b8defeb67ea65f318f9d08e6ccc53323eb4
Allison Oler <linoler@gmail.com>
2025-03-07 16:15:58 -0700

    Continuing to improve code quality

--- server/src/main/java/dataaccess/SqlAuthDOA.java
+++ server/src/main/java/dataaccess/SqlAuthDOA.java
@@ -3,6 +3,7 @@ package dataaccess;
 import model.AuthData;
 import model.UserData;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
@@ -17,22 +18,32 @@ public class SqlAuthDOA implements AuthDOA {
     private int executeUpdate(String statement, Object... params) throws DataAccessException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    if (param instanceof String p) ps.setString(i + 1, p);
-                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
-                    else if (param == null) ps.setNull(i + 1, NULL);
-                }
-                ps.executeUpdate();
-                var rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    return rs.getInt(1);
-                }
-                return 0;
+                return extracted(ps, params);
             }
         } catch (SQLException e) {
             throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
         }
+    }
+
+    private static int extracted(PreparedStatement ps, Object[] params) throws SQLException {
+        for (var i = 0; i < params.length; i++) {
+            var param = params[i];
+            if (param instanceof String p) {
+                ps.setString(i + 1, p);
+            }
+            else if (param instanceof Integer p) {
+                ps.setInt(i + 1, p);
+            }
+            else if (param == null) {
+                ps.setNull(i + 1, NULL);
+            }
+        }
+        ps.executeUpdate();
+        var rs = ps.getGeneratedKeys();
+        if (rs.next()) {
+            return rs.getInt(1);
+        }
+        return 0;
     }
 
     private final String[] createStatements = {
