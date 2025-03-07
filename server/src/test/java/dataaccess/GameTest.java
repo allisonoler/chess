c0621c4074a6d21655f07856429da58dd739a35e
Allison Oler <linoler@gmail.com>
2025-03-07 15:47:09 -0700

    Game tests halfway done, so far so good

--- server/src/test/java/dataaccess/GameTest.java
+++ server/src/test/java/dataaccess/GameTest.java
@@ -59,53 +59,53 @@ public class GameTest {
         }
     }
 
-//    @Test
-//    public void negativeInsertTest() throws DataAccessException {
-//        authDao.insertAuth(new AuthData("allison", "chocolate"));
-//        try {
-//            authDao.insertAuth(new AuthData("allison", "vanilla"));
-//        } catch (DataAccessException e){
-//            assertNotNull(e);
-//            try (var conn = DatabaseManager.getConnection()) {
-//                var statement = "SELECT username, authToken FROM auth WHERE username=?";
-//                try (var ps = conn.prepareStatement(statement)) {
-//                    ps.setString(1, "allison");
-//                    try (var rs= ps.executeQuery()) {
-//                        if (rs.next()) {
-//                            assertEquals(rs.getString("authToken"),"chocolate");
-//
-//                        }
-//                    }
-//                }
-//            } catch (Exception ex){
-//                throw new DataAccessException(String.format("Unable to read data: %s", ex.getMessage()));
-//            }
-//        }
-//    }
+    @Test
+    public void negativeInsertTest() throws DataAccessException {
+        gameDao.insertGame(new GameData("1", null, null, "fungame", new ChessGame()));
+        try {
+            gameDao.insertGame(new GameData("1", null, null, "fungame", new ChessGame()));
+        } catch (DataAccessException e){
+            assertNotNull(e);
+            try (var conn = DatabaseManager.getConnection()) {
+                var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM game WHERE gameID=?";
+                try (var ps = conn.prepareStatement(statement)) {
+                    ps.setString(1, "1");
+                    try (var rs= ps.executeQuery()) {
+                        if (rs.next()) {
+                            assertEquals(rs.getString("gameID"),"1");
 
-//    @Test
-//    public void positiveReadTest() throws DataAccessException {
-//        authDao.insertAuth(new AuthData("steve", "chocolate"));
-//        assertEquals(authDao.getAuth("chocolate").username(),"steve");
-//        assertEquals(authDao.getAuth("chocolate").authToken(),"chocolate");
-//    }
-//
-//    @Test
-//    public void negativeReadTest() throws DataAccessException {
-//        try {
-//            assertNull(authDao.getAuth("allison"));
-//        } catch (DataAccessException e){
-//            assertNotNull(e);
-//        }
-//    }
-//
-//    @Test
-//    public void positiveDeleteTest() throws DataAccessException {
-//        authDao.insertAuth(new AuthData("charlotte", "chocolate"));
-//        assertNotNull(authDao.getAuth("chocolate"));
-//        authDao.deleteAuth("chocolate");
-//        assertNull(authDao.getAuth("chocolate"));
-//    }
+                        }
+                    }
+                }
+            } catch (Exception ex){
+                throw new DataAccessException(String.format("Unable to read data: %s", ex.getMessage()));
+            }
+        }
+    }
+
+    @Test
+    public void positiveReadTest() throws DataAccessException {
+        gameDao.insertGame(new GameData("2", null, null, "fungame", new ChessGame()));
+        assertEquals(gameDao.getGame("2").gameName(),"fungame");
+        assertInstanceOf(ChessGame.class,gameDao.getGame("2").game());
+    }
+
+    @Test
+    public void negativeReadTest() throws DataAccessException {
+        try {
+            assertNull(gameDao.getGame("allison"));
+        } catch (DataAccessException e){
+            assertNotNull(e);
+        }
+    }
+
+    @Test
+    public void positiveDeleteTest() throws DataAccessException {
+        gameDao.insertGame(new GameData("3", null, null, "fungame", new ChessGame()));
+        assertNotNull(gameDao.getGame("3"));
+        gameDao.deleteGame("3");
+        assertNull(gameDao.getGame("3"));
+    }
 //
 //    @Test
 //    public void negativeDeleteTest() throws DataAccessException {
