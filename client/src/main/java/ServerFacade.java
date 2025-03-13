c73e7cc961cd451c79dbd05a4114387ccd185206
Allison Oler <linoler@gmail.com>
2025-03-12 23:22:54 -0600

    Set up phase 5

---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,18 @@
+import service.requestsresults.LoginRequest;
+import service.requestsresults.LoginResult;
+import service.requestsresults.RegisterRequest;
+import service.requestsresults.RegisterResult;
+
+public class ServerFacade {
+
+    //constructor and server url
+    public RegisterResult register(RegisterRequest request) {
+        return null;
+        //var path = "\pet";
+//        return this.makeRequest("POST", path, pet, Pet.class);
+    }
+
+    public LoginResult login(LoginRequest login) {
+        return null;
+    }
+}
